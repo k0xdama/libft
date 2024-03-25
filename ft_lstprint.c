48d203ba7870833f76d18c1a53258119cf7e14a2
Mateo PRIOUX <pmateo@paul-f4Br5s1.clusters.42paris.fr>
2024-03-25 04:08:11 +0100

    replace printf by ft_printf in ft_lstprint

--- ft_lstprint.c
+++ ft_lstprint.c
@@ -6,7 +6,7 @@
 /*   By: pmateo <pmateo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/25 02:52:47 by pmateo            #+#    #+#             */
-/*   Updated: 2024/03/25 02:53:24 by pmateo           ###   ########.fr       */
+/*   Updated: 2024/03/25 04:07:35 by pmateo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,14 @@ void	ft_lstprint(t_list *lst)
 {
 	while(lst)
 	{
-		printf("&cell = %p\n", lst);
+		ft_printf("&cell = %p\n", lst);
 		// % A CHANGER SELON LE TYPE DE DONNEES
-		printf("content_cell = %d\n", *((int *)lst->content));
-		printf("&next_cell = %p\n", lst->next);
-		printf("|\n|\n|\n");
+		ft_printf("content_cell = %d\n", *((int *)lst->content));
+		ft_printf("&next_cell = %p\n", lst->next);
+		ft_printf("|\n|\n|\n");
 		lst = lst->next;
 	}
-	printf("&cell = NULL\n");
-	printf("content_cell = NULL\n");
-	printf("&next_cell = NULL\n");
+	ft_printf("&cell = NULL\n");
+	ft_printf("content_cell = NULL\n");
+	ft_printf("&next_cell = NULL\n");
 }
